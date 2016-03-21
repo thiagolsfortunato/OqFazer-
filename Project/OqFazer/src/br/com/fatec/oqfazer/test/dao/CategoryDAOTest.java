742d145b778d21a06c77bbeef7b213dc5ad71eda
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-21 16:47:20 -0300

    Update Class CategoryDAOTest from test.dao

--- Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/CategoryDAOTest.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/CategoryDAOTest.java
@@ -1,5 +1,25 @@
 package br.com.fatec.oqfazer.test.dao;
 
-public class CategoryDAOTest {
+import org.junit.Before;
 
+import br.com.fatec.oqfazer.api.dao.CategoryDAO;
+import br.com.fatec.oqfazer.test.common.TestBase;
+import br.com.spektro.minispring.core.implfinder.ImplFinder;
+
+public class CategoryDAOTest extends TestBase{
+	
+	private CategoryDAO dao;
+	
+	@Before
+	public void config() {
+		this.dao = ImplFinder.getImpl(CategoryDAO.class);
+	}
+	
+	public void testSave(){}
+	
+	public void testUpdate(){}
+	
+	public void testDelete(){}
+	
+	public void testFindAll(){}
 }
