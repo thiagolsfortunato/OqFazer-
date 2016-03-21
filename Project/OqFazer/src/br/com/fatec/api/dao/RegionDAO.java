ef647348127b4871bc6dde7fba6c8a40c0a5e07f
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-21 14:16:50 -0300

    Update class RegionDAO from api.dao

--- Project/OqFazer/src/br/com/fatec/api/dao/RegionDAO.java
+++ Project/OqFazer/src/br/com/fatec/api/dao/RegionDAO.java
@@ -1,5 +1,17 @@
 package br.com.fatec.api.dao;
 
-public interface RegionDAO {
+import java.util.List;
+import br.com.fatec.api.entity.Region;
 
+public interface RegionDAO {
+	
+	Long insertRegion(Region region);
+	
+	Long deleteRegion(Long id);
+	
+	void updateRegion(Region region);
+	
+	Region searchRegionById(Long id);
+	
+	List<Region> searchAllRegion();
 }
