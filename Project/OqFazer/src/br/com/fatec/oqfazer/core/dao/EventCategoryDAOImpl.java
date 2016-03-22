c90507a60ec7b3fabe3cfdb31ceba489a8462dfb
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-22 20:01:59 -0300

    Create Class EventCategoryDAOImpl

---
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/EventCategoryDAOImpl.java
@@ -0,0 +1,27 @@
+package br.com.fatec.oqfazer.core.dao;
+
+import java.util.List;
+import br.com.fatec.oqfazer.api.dao.EventCategory;
+import br.com.fatec.oqfazer.api.entity.Category;
+
+public class EventCategoryDAOImpl implements EventCategory {
+
+	@Override
+	public void updateCategory(Long eventId, List<Category> category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCategory(Long eventId, Category category) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Long> searchCategory(Long eventId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
