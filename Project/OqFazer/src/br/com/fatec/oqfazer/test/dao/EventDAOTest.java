46e4cc4b7fc00eee9aa22f75034a27b11e873c9c
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-22 14:08:30 -0300

    Update Class RegionDAOTest

--- Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/EventDAOTest.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/EventDAOTest.java
@@ -1,13 +1,19 @@
 package br.com.fatec.oqfazer.test.dao;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import br.com.fatec.oqfazer.api.dao.CategoryDAO;
 import br.com.fatec.oqfazer.api.dao.EventDAO;
+import br.com.fatec.oqfazer.api.dao.RegionDAO;
 import br.com.fatec.oqfazer.api.dao.UserDAO;
 import br.com.fatec.oqfazer.api.entity.Category;
+import br.com.fatec.oqfazer.api.entity.City;
+
 import br.com.fatec.oqfazer.api.entity.Event;
+import br.com.fatec.oqfazer.api.entity.Region;
 import br.com.fatec.oqfazer.api.entity.User;
 import br.com.fatec.oqfazer.test.common.TestBase;
 import br.com.spektro.minispring.core.implfinder.ImplFinder;
@@ -17,38 +23,55 @@ public class EventDAOTest extends TestBase {
 	private EventDAO eventDAO;
 	private UserDAO userDAO;
 	private CategoryDAO categoryDAO;
+	private RegionDAO regionDAO;
 	
 	@Before
 	public void config() {
 		this.eventDAO = ImplFinder.getImpl(EventDAO.class);
 		this.userDAO = ImplFinder.getImpl(UserDAO.class);
 		this.categoryDAO = ImplFinder.getImpl(CategoryDAO.class);
+		this.regionDAO = ImplFinder.getFinalImpl(RegionDAO.class);
 		
 		User user = new User();
+		user.setId((long) 1);
 		user.setName("User 1");
 		user.setEmail("user1@test");
 		user.setPassword("user1");
 		user.setPhone(11111);
 		
 		User user2 = new User();
+		user2.setId((long) 2);
 		user2.setName("User 2");
 		user2.setEmail("user2@test");
 		user2.setPassword("user2");
 		user2.setPhone(22222);
 		
 		User user3 = new User();
+		user3.setId((long) 3);
 		user3.setName("User 3");
 		user3.setEmail("user3@test");
 		user3.setPassword("user3");
 		user3.setPhone(3333);
 		
 		Category category1 = new Category();
+		category1.setId((long) 1);
 		category1.setName("Show");
 		category1.setCategory(null);
 		
 		Category category2 = new Category();
+		category1.setId((long) 1);
 		category2.setName("Show Rock");
 		category2.setCategory(category1);
+		
+		List<City> cities = null;
+		for(City city : City.values()){
+			cities.add(city);
+		}
+		
+		Region region1 = new Region();
+		region1.setId((long) 1);
+		region1.setName("Vale do Paraiba");
+		region1.setCity(cities);
 	}
 	
 	@Test
--- Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/RegionDAOTest.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/RegionDAOTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.fatec.oqfazer.api.dao.RegionDAO;
+import br.com.fatec.oqfazer.api.entity.City;
 import br.com.fatec.oqfazer.api.entity.Region;
 import br.com.fatec.oqfazer.test.common.TestBase;
 import br.com.spektro.minispring.core.implfinder.ImplFinder;
@@ -22,44 +23,69 @@ public class RegionDAOTest extends TestBase{
 	
 	@Test
 	public void testSave(){
+		List<City> cities = null;
+		cities.add(City.CACAPAVA);
+		cities.add(City.JACAREI);
+		
 		Region saveRegion = new Region();		
 		saveRegion.setId((long) 1);
-		saveRegion.setName("Caçapava");
+		saveRegion.setName("Vale do Paraiba");
+		saveRegion.setCity(cities);		
 		
 		Long id = this.dao.insertRegion(saveRegion);
 		Region savedRegion = this.dao.searchRegionById(id);
 		
 		Assert.assertNotNull(savedRegion);
 		Assert.assertEquals(String.valueOf(1), savedRegion.getId());
-		Assert.assertEquals("Caçapava", savedRegion.getName());
+		Assert.assertEquals("Vale do Paraiba", savedRegion.getName());
+		Assert.assertEquals(City.CACAPAVA, savedRegion.getCity().get(0));
+		Assert.assertEquals(City.JACAREI, savedRegion.getCity().get(1));
+		
 	}
 	
 	@Test
 	public void testUpdate() {
+		List<City> cities = null;
+		cities.add(City.CACAPAVA);
+		cities.add(City.TAUBATE);
+		
 		Region region = new Region();
 		region.setId((long) 1);
-		region.setName("Caçapava");
+		region.setName("Sentido Rio");
+		region.setCity(cities);
 
 		Long id = this.dao.insertRegion(region);
 		Region updateRegion = this.dao.searchRegionById(id);
 
+		List<City> cities2 = null;
+		cities2.add(City.SAO_JOSE_DOS_CAMPOS);
+		cities2.add(City.JACAREI);
+				
 		updateRegion.setId((long)1);
-		updateRegion.setName("Kansaspava");
+		updateRegion.setName("Sentido Sao Paulo");
+		updateRegion.setCity(cities2);		
 
 		this.dao.updateRegion(updateRegion);;
 		Region updatedRegion = this.dao.searchRegionById(id);
 
 		Assert.assertNotNull(updatedRegion);
 		Assert.assertEquals(String.valueOf(1), updatedRegion.getId());
-		Assert.assertEquals("kansaspava", updatedRegion.getName());
+		Assert.assertEquals("Sentido Sao Paulo", updatedRegion.getName());
+		Assert.assertEquals(City.SAO_JOSE_DOS_CAMPOS, updateRegion.getCity().get(0));
+		Assert.assertEquals(City.TAUBATE, updateRegion.getCity().get(1));
 	}
 	
 	@Test
 	public void testDelete() {
+		List<City> cities = null;
+		cities.add(City.CACAPAVA);
+		cities.add(City.TAUBATE);
+		
 		Region region = new Region();
 		region.setId((long)1);
-		region.setName("Caçapava");
-
+		region.setName("Vale do Paraiba");
+		region.setCity(cities);
+		
 		Long id = this.dao.insertRegion(region);
 		this.dao.deleteRegion(id);;
 
@@ -70,12 +96,23 @@ public class RegionDAOTest extends TestBase{
 	
 	@Test
 	public void testFindAll() {
+		List<City> cities = null;
+		cities.add(City.CACAPAVA);
+		cities.add(City.TAUBATE);
+		
 		Region region1 = new Region();
 		region1.setId((long) 1);
-		region1.setName("Caçapava");
+		region1.setName("Sentido Rio");
+		region1.setCity(cities);
+		
+		List<City> cities2 = null;
+		cities2.add(City.SAO_JOSE_DOS_CAMPOS);
+		cities2.add(City.JACAREI);
+		
 		Region region2 = new Region();
-		region2.setId((long)2);
-		region2.setName("São José dos Campos");
+		region2.setId((long) 2);
+		region2.setName("Sentido Sao Paulo");
+		region2.setCity(cities2);
 
 		this.dao.insertRegion(region1);
 		this.dao.insertRegion(region2);
@@ -83,10 +120,16 @@ public class RegionDAOTest extends TestBase{
 		List<Region> listRegions = this.dao.searchAllRegions();
 
 		Assert.assertEquals(2, listRegions.size());
-		Assert.assertEquals("user 1", listRegions.get(0).getId());
-		Assert.assertEquals("password_1", listRegions.get(0).getName());
-		Assert.assertEquals("user 2", listRegions.get(1).getId());
-		Assert.assertEquals("password_2", listRegions.get(1).getName());
+		Assert.assertEquals(String.valueOf(1), listRegions.get(0).getId());
+		Assert.assertEquals("Sentido Rio", listRegions.get(0).getName());
+		Assert.assertEquals(City.CACAPAVA, listRegions.get(0).getCity().get(0));
+		Assert.assertEquals(City.TAUBATE, listRegions.get(0).getCity().get(1));
+		
+		Assert.assertEquals(String.valueOf(2), listRegions.get(1).getId());
+		Assert.assertEquals("Sentido Sao Paulo", listRegions.get(1).getName());
+		Assert.assertEquals(City.SAO_JOSE_DOS_CAMPOS, listRegions.get(1).getCity().get(0));
+		Assert.assertEquals(City.JACAREI, listRegions.get(1).getCity().get(1));
 	}
 	
 }
+
