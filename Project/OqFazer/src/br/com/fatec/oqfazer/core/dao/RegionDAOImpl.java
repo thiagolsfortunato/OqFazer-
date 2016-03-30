dda019d7aade8245c753ee146b0bd6016bdf28ba
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-30 14:18:31 -0300

    Update Class RegionDAOImpl
    Create methods Insert, Update, Delete and Seach in the class

--- Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/RegionDAOImpl.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/RegionDAOImpl.java
@@ -3,9 +3,12 @@ package br.com.fatec.oqfazer.core.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.commons.dbutils.DbUtils;
+
+import com.google.common.collect.Lists;
 
 import static br.com.spektro.minispring.core.dbmapper.ConfigDBMapper.getDefaultConnectionType;
 import br.com.fatec.oqfazer.api.dao.RegionDAO;
@@ -47,26 +50,90 @@ public class RegionDAOImpl implements RegionDAO{
 
 	@Override
 	public void deleteRegion(Long id) {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		PreparedStatement delete = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "DELETE FROM "+ Region.TABLE + " WHERE "+ Region.COL_ID +" = ?";
+			delete = conn.prepareStatement(sql);
+			delete.setLong(1, id);
+			delete.execute();
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(delete);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
 	public void updateRegion(Region region) {
-		// TODO Auto-generated method stub
-		
+		Connection conn = null;
+		PreparedStatement update = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "UPDATE "+Region.TABLE+" SET "+ Region.COL_NAME +" = ? WHERE "+ Region.COL_ID +" = ?";
+			update = conn.prepareStatement(sql);
+			update.setString(1, region.getName());
+			update.setLong(2, region.getId());
+			update.execute();
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(update);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
 	public Region searchRegionById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement find = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "SELECT * FROM "+ Region.TABLE +" WHERE "+ Region.COL_ID +" = ?";
+			find = conn.prepareStatement(sql);
+			ResultSet rs = find.executeQuery();
+			return buildRegion(rs);
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(find);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
 	public List<Region> searchAllRegions() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement findAll = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "SELECT * FROM "+ Region.TABLE +" ORDER BY "+ Region.COL_ID;
+			findAll = conn.prepareStatement(sql);
+			ResultSet rs = findAll.executeQuery();
+			return buildRegions(rs);
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(findAll);
+			DbUtils.closeQuietly(conn);
+		}
+	}
+	
+	public Region buildRegion(ResultSet rs) throws SQLException{
+		Region region = new Region();
+		region.setId(rs.getLong(Region.COL_ID));
+		region.setName(rs.getString(Region.COL_NAME));
+		return region;
+	}
+	
+	public List<Region> buildRegions(ResultSet rs) throws SQLException{
+		List<Region> regions = Lists.newArrayList();
+		while(rs.next()){
+			regions.add(buildRegion(rs));
+		}
+		return regions;
 	}
 
 }
