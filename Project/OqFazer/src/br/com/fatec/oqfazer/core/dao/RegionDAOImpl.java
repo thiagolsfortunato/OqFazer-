9878cac8af7c924f57b7b31fcb15519893fae828
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-30 10:45:07 -0300

    Update RegionDAOImpl
    Create method of the insert

--- Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/RegionDAOImpl.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/RegionDAOImpl.java
@@ -1,16 +1,48 @@
 package br.com.fatec.oqfazer.core.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import static br.com.spektro.minispring.core.dbmapper.ConfigDBMapper.getDefaultConnectionType;
 import br.com.fatec.oqfazer.api.dao.RegionDAO;
 import br.com.fatec.oqfazer.api.entity.Region;
+import br.com.spektro.minispring.core.dbmapper.ConfigDBMapper;
+
 
 public class RegionDAOImpl implements RegionDAO{
 
 	@Override
 	public Long insertRegion(Region region) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement insert = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String columns = DAOUtils.getColumns(getDefaultConnectionType(), Region.getColumns());
+			String values = DAOUtils.completeClauseValues(getDefaultConnectionType(), Region.getColumns().size(), "SEQ_REGION");
+			
+			String sql = "INSERT INTO "+ Region.TABLE + columns + " VALUES " + values;
+			
+			insert = DAOUtils.buildStatment(sql, conn, getDefaultConnectionType(), Region.getColumnsArray());
+			
+			insert.setLong(1, region.getId());
+			insert.setString(2, region.getName());
+			insert.execute();
+			
+			ResultSet generatedKeys = insert.getGeneratedKeys();
+			if (generatedKeys.next()) {
+				return generatedKeys.getLong(1);
+			}
+			return null;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(insert);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
