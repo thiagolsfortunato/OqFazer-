701ec5966bcd6929eb96c0c6a5e3748b7e27e592
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-31 16:18:45 -0300

    Implementation of the Class EventCategoryDAOImpl

--- Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/EventCategoryDAOImpl.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/dao/EventCategoryDAOImpl.java
@@ -1,28 +1,137 @@
 package br.com.fatec.oqfazer.core.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Collections;
 import java.util.List;
+
+import org.apache.commons.dbutils.DbUtils;
+
+import com.google.common.collect.Lists;
+
 import br.com.fatec.oqfazer.api.dao.EventCategory;
 import br.com.fatec.oqfazer.api.entity.Category;
+import br.com.spektro.minispring.core.dbmapper.ConfigDBMapper;
 
 public class EventCategoryDAOImpl implements EventCategory {
-
+	
+	public static final String TABLE = "EVENT_CATEGORY";
+	public static final String COL_ID_EVENT = "ECT_EVENT_ID";
+	public static final String COL_ID_CATEGORY = "ECT_CATEGORY_ID";
+	
+	
 	@Override
-	public void updateCategory(Long eventId, List<Category> categories) {
-		// TODO Auto-generated method stub
-		
+	public Long insertEventCategory(Long idEvent, Category category) {
+		Connection conn = null;
+		PreparedStatement insert = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "INSERT INTO "+ TABLE +" VALUES (?,?)";
+			insert = conn.prepareStatement(sql);
+			insert.setLong(1, idEvent);
+			insert.setLong(1, category.getId());
+			insert.execute();
+			return idEvent;
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(insert);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
-	public void updateCategory(Long eventId, Category category) {
-		// TODO Auto-generated method stub
-		
+	public Long insertEventCategory(Long idEvent, List<Category> categories) {
+		Connection conn = null;
+		PreparedStatement insert = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			for(Category category : categories){
+				String sql = "INSERT INTO "+ TABLE +" VALUES (?,?)";
+				insert = conn.prepareStatement(sql);
+				insert.setLong(1, idEvent);
+				insert.setLong(1, category.getId());
+				insert.execute();
+			}			
+			return idEvent;
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(insert);
+			DbUtils.closeQuietly(conn);
+		}
 	}
 
 	@Override
-	public List<Category> searchCategory(Long eventId) {
-		// TODO Auto-generated method stub
-		return null;
+	public void updateEventCategory(Long idEvent, List<Category> categories) {
+		if(categories != null){
+			categories.removeAll(Collections.singleton(null));
+			Connection conn = ConfigDBMapper.getDefaultConnection();
+			PreparedStatement delete = null;
+			PreparedStatement insert = null;
+			try{
+				String sqlDelete = "DELETE FROM "+ TABLE +" WHERE "+ COL_ID_EVENT +" =?";
+				delete = conn.prepareStatement(sqlDelete);
+				delete.setLong(1, idEvent);
+				delete.execute();
+				delete.close();
+				
+				for(Category category : categories){
+					String sqlInsert = "INSERT INTO "+ TABLE +" VALUES (?,?)";
+					insert = conn.prepareStatement(sqlInsert);
+					insert.setLong(1, idEvent);
+					insert.setLong(2, category.getId());
+					insert.execute();
+				}
+			}catch (Exception e) {
+				throw new RuntimeException(e);
+			} finally {
+				DbUtils.closeQuietly(insert);
+				DbUtils.closeQuietly(delete);
+				DbUtils.closeQuietly(conn);
+			}
+		}	
 	}
 
+	@Override
+	public void deleteEventCategory(Long idEvent, Long idCategory) {
+		Connection conn = null;
+		PreparedStatement delete = null;
+		try{
+			conn = ConfigDBMapper.getDefaultConnection();
+			String sql = "DELETE FROM "+ TABLE +" WHERE "+ COL_ID_EVENT +" =? and "+ COL_ID_CATEGORY +" = ?";
+			delete = conn.prepareStatement(sql);
+			delete.setLong(1, idEvent);
+			delete.setLong(2, idCategory);
+			delete.execute();
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(delete);
+			DbUtils.closeQuietly(conn);
+		}
+	}
 
+	@Override
+	public List<Long> searchCategories(Long idEvent) {
+		Connection conn = null;
+		PreparedStatement select = null;
+		List<Long> idsCategories = Lists.newArrayList();
+		try{
+			String sql = "SELECT "+ COL_ID_CATEGORY +" FROM "+ TABLE + " WHERE "+ COL_ID_EVENT +" = ?";
+			select = conn.prepareStatement(sql);
+			select.setLong(1, idEvent);
+			ResultSet rs = select.executeQuery();
+			while(rs.next()){
+				idsCategories.add(rs.getLong(COL_ID_CATEGORY));
+			}
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			DbUtils.closeQuietly(select);
+			DbUtils.closeQuietly(conn);
+		}
+		return idsCategories;
+	}
 }
