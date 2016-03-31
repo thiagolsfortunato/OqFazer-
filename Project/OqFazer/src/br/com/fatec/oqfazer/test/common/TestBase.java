e55cd4ead85259e911c5be4d41387454accf3ead
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-03-31 20:19:53 -0300

    Create new Classes about Database
    Creation context Hsql, MySql, Oracle, Postgres

--- Project/OqFazer/src/br/com/fatec/oqfazer/test/common/TestBase.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/common/TestBase.java
@@ -2,6 +2,12 @@ package br.com.fatec.oqfazer.test.common;
 
 import org.junit.After;
 import org.junit.BeforeClass;
+
+import br.com.fatec.oqfazer.api.entity.Category;
+import br.com.fatec.oqfazer.api.entity.City;
+import br.com.fatec.oqfazer.api.entity.Event;
+import br.com.fatec.oqfazer.api.entity.Region;
+import br.com.fatec.oqfazer.api.entity.User;
 import br.com.spektro.minispring.core.dbmapper.ConfigDBMapper;
 import br.com.spektro.minispring.core.implfinder.ContextSpecifier;
 import br.com.spektro.minispring.core.liquibaseRunner.LiquibaseRunnerService;
@@ -19,19 +25,18 @@ public class TestBase {
 	
 	@After
 	public void setDown() {
-		QueryExecutorService.executeQuery("DELETE FROM ");
-		QueryExecutorService.executeQuery("DELETE FROM ");
-		QueryExecutorService.executeQuery("DELETE FROM ");
-		QueryExecutorService.executeQuery("DELETE FROM ");
-		QueryExecutorService.executeQuery("DELETE FROM ");
+		QueryExecutorService.executeQuery("DELETE FROM "+Category.TABLE);
+		QueryExecutorService.executeQuery("DELETE FROM "+City.TABLE);
+		QueryExecutorService.executeQuery("DELETE FROM "+Event.TABLE);
+		QueryExecutorService.executeQuery("DELETE FROM "+Region.TABLE);
+		QueryExecutorService.executeQuery("DELETE FROM "+User.TABLE);
+		QueryExecutorService.executeQuery("DELETE FROM PARTICIPATION");
+		QueryExecutorService.executeQuery("DELETE FROM EVENT_CATEGORY");
 
-		QueryExecutorService
-				.executeQuery("ALTER SEQUENCE SEQ_PROJETO_PAPEL RESTART WITH 1");
-		QueryExecutorService
-				.executeQuery("ALTER SEQUENCE SEQ_PROJETO_GRUPO_PAPEL RESTART WITH 1");
-		QueryExecutorService
-				.executeQuery("ALTER SEQUENCE SEQ_PROJETO_USUARIO RESTART WITH 1");
-		;
+		QueryExecutorService.executeQuery("ALTER SEQUENCE SEQ_CATEGORY RESTART WITH 1");
+		QueryExecutorService.executeQuery("ALTER SEQUENCE SEQ_CITY RESTART WITH 1");
+		QueryExecutorService.executeQuery("ALTER SEQUENCE SEQ_EVENT RESTART WITH 1");
+		QueryExecutorService.executeQuery("ALTER SEQUENCE SEQ_USER RESTART WITH 1");
 	}
 	
 }
--- Project/OqFazer/src/br/com/fatec/oqfazer/test/multidb/PostgresTest.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/multidb/PostgresTest.java
@@ -3,17 +3,16 @@ package br.com.fatec.oqfazer.test.multidb;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import br.com.fatec.oqfazer.test.dao.UserDAOTest;
 import br.com.spektro.minispring.core.dbmapper.ConfigDBMapper;
 import br.com.spektro.minispring.core.implfinder.ContextSpecifier;
 import br.com.spektro.minispring.core.liquibaseRunner.LiquibaseRunnerService;
 
-public class MySQLTest {
-
+public class PostgresTest {
+	
 	@BeforeClass
 	public static void setUp() {
 		ContextSpecifier.setContext("br.com.fatec.oqfazer");
-		ConfigDBMapper.setDefaultConnectionName("mysql");
+		ConfigDBMapper.setDefaultConnectionName("postgres");
 		LiquibaseRunnerService.run();
 	}
 
