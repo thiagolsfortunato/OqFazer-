46221e16e21b5f9d3e59c0f0106882c80d6ac193
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-04 10:18:11 -0300

    Update UserDAOTest, Working !

--- Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/UserDAOTest.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/test/dao/UserDAOTest.java
@@ -23,7 +23,7 @@ public class UserDAOTest extends TestBase{
 	@Test
 	public void testSave(){
 		User saveUser = new User();
-		saveUser.setId((long)1);
+		saveUser.setId((long) 1);
 		saveUser.setName("william");
 		saveUser.setEmail("williamcezart@gmail.com");
 		saveUser.setPassword("facebook");
@@ -33,8 +33,8 @@ public class UserDAOTest extends TestBase{
 		User savedUser = this.dao.searchUserById(id);
 		
 		Assert.assertNotNull(savedUser);
-		Assert.assertEquals(String.valueOf(1), savedUser.getId());
-		Assert.assertEquals("William", savedUser.getName());
+		Assert.assertEquals(new Long(1), savedUser.getId());
+		Assert.assertEquals("william", savedUser.getName());
 		Assert.assertEquals("williamcezart@gmail.com", savedUser.getEmail());
 		Assert.assertEquals("facebook", savedUser.getPassword());
 		Assert.assertEquals(36535959, savedUser.getPhone());
@@ -43,7 +43,7 @@ public class UserDAOTest extends TestBase{
 	@Test
 	public void testUpdate() {
 		User user = new User();
-		user.setId((long)1);
+		user.setId((long) 1);
 		user.setName("william");
 		user.setEmail("williamcezart@gmail.com");
 		user.setPassword("facebook");
@@ -59,7 +59,7 @@ public class UserDAOTest extends TestBase{
 		User atualizedUser = this.dao.searchUserById(id);
 
 		Assert.assertNotNull(atualizedUser);
-		Assert.assertEquals(String.valueOf(1), atualizedUser.getId());
+		Assert.assertEquals(new Long (1), atualizedUser.getId());
 		Assert.assertEquals("william penna", atualizedUser.getName());
 		Assert.assertEquals("williamcezart@gmail.com", atualizedUser.getEmail());
 		Assert.assertEquals("nova_senha", atualizedUser.getPassword());
@@ -69,7 +69,7 @@ public class UserDAOTest extends TestBase{
 	@Test
 	public void testDelete() {
 		User user = new User();
-		user.setId((long)1);
+		user.setId((long) 1);
 		user.setName("william");
 		user.setEmail("williamcezart@gmail.com");
 		user.setPassword("facebook");
@@ -86,14 +86,14 @@ public class UserDAOTest extends TestBase{
 	@Test
 	public void testFindAll() {
 		User user1 = new User();
-		user1.setId((long)1);
+		user1.setId((long) 1);
 		user1.setName("william");
 		user1.setEmail("williamcezart@gmail.com");
 		user1.setPassword("facebook");
 		user1.setPhone(36535959);
 		
 		User user2 = new User();
-		user2.setId((long)1);
+		user2.setId((long) 2);
 		user2.setName("thiago");
 		user2.setEmail("thiago@test");
 		user2.setPassword("teste");
@@ -105,16 +105,16 @@ public class UserDAOTest extends TestBase{
 		List<User> listUsers = this.dao.searchAllUsers();
 
 		Assert.assertEquals(2, listUsers.size());
-		Assert.assertEquals(String.valueOf(1), listUsers.get(0).getId());
-		Assert.assertEquals("william penna", listUsers.get(0).getName());
+		Assert.assertEquals(new Long (1), listUsers.get(0).getId());
+		Assert.assertEquals("william", listUsers.get(0).getName());
 		Assert.assertEquals("williamcezart@gmail.com", listUsers.get(0).getEmail());
-		Assert.assertEquals("nova_senha", listUsers.get(0).getPassword());
+		Assert.assertEquals("facebook", listUsers.get(0).getPassword());
 		Assert.assertEquals(36535959, listUsers.get(0).getPhone());
 		
-		Assert.assertEquals(String.valueOf(2), listUsers.get(1).getId());
+		Assert.assertEquals(new Long (2), listUsers.get(1).getId());
 		Assert.assertEquals("thiago", listUsers.get(1).getName());
 		Assert.assertEquals("thiago@test", listUsers.get(1).getEmail());
-		Assert.assertEquals("test", listUsers.get(1).getPassword());
+		Assert.assertEquals("teste", listUsers.get(1).getPassword());
 		Assert.assertEquals(111111, listUsers.get(1).getPhone());
 	}
 		
