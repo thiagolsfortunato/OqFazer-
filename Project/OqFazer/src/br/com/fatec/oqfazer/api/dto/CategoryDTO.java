54a500ed6a0213d642af6fd0a1050b2a74e82a50
williampenna <williamcezart@gmail.com>
2016-04-12 06:20:17 -0300

    Update Classes api/dto

--- Project/OqFazer/src/br/com/fatec/oqfazer/api/dto/CategoryDTO.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/api/dto/CategoryDTO.java
@@ -1,5 +1,47 @@
 package br.com.fatec.oqfazer.api.dto;
 
-public class CategoryDTO {
+import br.com.fatec.oqfazer.api.entity.Category;
 
+public class CategoryDTO {
+	
+	private Long id;
+	private String name;
+	private Category category;
+	
+	public CategoryDTO(){};
+	
+	public CategoryDTO(Long id, String name, Category category) {
+		this.id = id;
+		this.name = name;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	public String toString(){
+		return "Category[" + this.id + " - " + this.name + " ]";
+	}
+	
 }
--- Project/OqFazer/src/br/com/fatec/oqfazer/api/dto/UserDTO.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/api/dto/UserDTO.java
@@ -1,5 +1,98 @@
 package br.com.fatec.oqfazer.api.dto;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 public class UserDTO {
+	
+	private Long id;
+	private String name;
+	private String password;
+	private String email;
+	private int phone;
+	private List<EventDTO> eventos = Lists.newArrayList();
+	
+	private Boolean isAdmin;
+	private Long startSession;
+	
+	public UserDTO (){
+	}
+	
+	public UserDTO (Long id, String name, String password, String email, int phone){
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public List<EventDTO> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(List<EventDTO> eventos) {
+		this.eventos = eventos;
+	}
+	
+	public String toString(){
+		return "User[" + this.id + " - " + this.name + "]";
+	}
+
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public Long getStartSession() {
+		return startSession;
+	}
+
+	public void setStartSession(Long startSession) {
+		this.startSession = startSession;
+	}
 
 }
