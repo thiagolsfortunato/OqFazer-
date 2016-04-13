fe160e0ca59731d6658ba2a519c19ea963a025f2
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-13 14:39:13 -0300

    Update RegionDTOConverter interface

--- Project/OqFazer/src/br/com/fatec/oqfazer/core/converter/RegionDTOConverter.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/converter/RegionDTOConverter.java
@@ -1,5 +1,35 @@
 package br.com.fatec.oqfazer.core.converter;
 
-public class RegionDTOConverter {
+import java.util.List;
+
+import br.com.fatec.oqfazer.api.dto.RegionDTO;
+import br.com.fatec.oqfazer.api.entity.Region;
+import br.com.spektro.minispring.dto.DTOConverter;
+
+public class RegionDTOConverter implements DTOConverter<Region, RegionDTO> {
+
+	@Override
+	public RegionDTO toDTO(Region region) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RegionDTO> toDTO(List<Region> regions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Region toEntity(RegionDTO regionDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Region> toEntity(List<RegionDTO> regionsDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
