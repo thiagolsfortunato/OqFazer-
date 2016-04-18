6fda87d6f2f0c1ed90099f4e72424d806f162b02
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-18 17:34:40 -0300

    Update Services
    CategoryService
    UserService

--- Project/OqFazer/src/br/com/fatec/oqfazer/api/service/CategoryService.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/api/service/CategoryService.java
@@ -1,5 +1,18 @@
 package br.com.fatec.oqfazer.api.service;
 
-public interface CategoryService {
+import java.util.List;
 
+import br.com.fatec.oqfazer.api.dto.CategoryDTO;
+
+public interface CategoryService {
+	
+	CategoryDTO insert(CategoryDTO categoryDTO);
+	
+	void update(CategoryDTO categoryDTO);
+	
+	void delete(Long idCategoryDTO);
+	
+	List<CategoryDTO> searchAll();
+	
+	CategoryDTO searchById(Long idCategoryDTO);
 }
