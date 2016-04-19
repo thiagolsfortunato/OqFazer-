ec3a661ce74df460950c9705096d055b3d500f32
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-19 15:35:41 -0300

    Update Category and Region Service Implements

--- Project/OqFazer/src/br/com/fatec/oqfazer/core/service/CategoryServiceImpl.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/service/CategoryServiceImpl.java
@@ -2,39 +2,50 @@ package br.com.fatec.oqfazer.core.service;
 
 import java.util.List;
 
+import br.com.fatec.oqfazer.api.dao.CategoryDAO;
 import br.com.fatec.oqfazer.api.dto.CategoryDTO;
+import br.com.fatec.oqfazer.api.entity.Category;
 import br.com.fatec.oqfazer.api.service.CategoryService;
+import br.com.fatec.oqfazer.core.converter.CategoryDTOConverter;
+import br.com.spektro.minispring.core.implfinder.ImplFinder;
 
 public class CategoryServiceImpl implements CategoryService{
 
+	private CategoryDAO categoryDAO;
+	private CategoryDTOConverter categoryDTOConveter;
+	
+	public CategoryServiceImpl() {
+		this.categoryDAO = ImplFinder.getImpl(CategoryDAO.class);
+		this.categoryDTOConveter = ImplFinder.getFinalImpl(CategoryDTOConverter.class);
+	}
+	
+	
 	@Override
 	public CategoryDTO insert(CategoryDTO categoryDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		Category category = this.categoryDTOConveter.toEntity(categoryDTO);
+		Long id = categoryDAO.insertCategory(category);
+		categoryDTO.setId(id);
+		return categoryDTO;
 	}
 
 	@Override
 	public void update(CategoryDTO categoryDTO) {
-		// TODO Auto-generated method stub
-		
+		Category category = this.categoryDTOConveter.toEntity(categoryDTO);
+		this.categoryDAO.updateCategory(category);
 	}
 
 	@Override
 	public void delete(Long idCategoryDTO) {
-		// TODO Auto-generated method stub
-		
+		this.categoryDAO.deleteCategory(idCategoryDTO);
 	}
 
 	@Override
 	public List<CategoryDTO> searchAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.categoryDTOConveter.toDTO(this.categoryDAO.searchAllCategory());
 	}
 
 	@Override
 	public CategoryDTO searchById(Long idCategoryDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.categoryDTOConveter.toDTO(this.categoryDAO.searchCategoryById(idCategoryDTO));
 	}
-
 }
--- Project/OqFazer/src/br/com/fatec/oqfazer/core/service/RegionServiceImpl.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/core/service/RegionServiceImpl.java
@@ -2,39 +2,50 @@ package br.com.fatec.oqfazer.core.service;
 
 import java.util.List;
 
+import br.com.fatec.oqfazer.api.dao.RegionDAO;
 import br.com.fatec.oqfazer.api.dto.RegionDTO;
+import br.com.fatec.oqfazer.api.entity.Region;
 import br.com.fatec.oqfazer.api.service.RegionService;
+import br.com.fatec.oqfazer.core.converter.RegionDTOConverter;
+import br.com.spektro.minispring.core.implfinder.ImplFinder;
 
 public class RegionServiceImpl implements RegionService{
-
+	
+	private RegionDAO regionDAO;
+	private RegionDTOConverter regionDTOConverter;
+	
+	public RegionServiceImpl() {
+		this.regionDAO = ImplFinder.getImpl(RegionDAO.class);
+		this.regionDTOConverter = ImplFinder.getFinalImpl(RegionDTOConverter.class);
+	}
+	
 	@Override
 	public RegionDTO insert(RegionDTO regionDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		Region region = this.regionDTOConverter.toEntity(regionDTO);
+		Long id = this.regionDAO.insertRegion(region);
+		regionDTO.setId(id);
+		return regionDTO;
 	}
 
 	@Override
 	public void update(RegionDTO regionDTO) {
-		// TODO Auto-generated method stub
-		
+		Region region = this.regionDTOConverter.toEntity(regionDTO);
+		this.regionDAO.updateRegion(region);
 	}
 
 	@Override
 	public void delete(Long idRegionDTO) {
-		// TODO Auto-generated method stub
-		
+		this.regionDAO.deleteRegion(idRegionDTO);
 	}
 
 	@Override
 	public List<RegionDTO> searchAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.regionDTOConverter.toDTO(this.regionDAO.searchAllRegions());
 	}
 
 	@Override
 	public RegionDTO searchById(Long idRegionDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.regionDTOConverter.toDTO(this.regionDAO.searchRegionById(idRegionDTO));
 	}
 
 }
