94418a4166911285d43a1946f0dbd37ce9b542c7
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-23 18:21:01 -0300

    Implementation Filter1 and Filter2

--- Project/OqFazer/src/br/com/fatec/oqfazer/web/filter/Filter1.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/web/filter/Filter1.java
@@ -1,5 +1,31 @@
 package br.com.fatec.oqfazer.web.filter;
 
-public class Filter1 {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Filter1 implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+		resp.getWriter().println("Filtro 1 - IDA");
+		chain.doFilter(req, resp);
+		resp.getWriter().println("Filtro 1 - VOTA");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
 
 }
