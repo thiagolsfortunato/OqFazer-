4221a1bdf677c6b72d6f7617c6677f158b071bc8
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-23 18:28:36 -0300

    Implementation Servelet
    Servelet1
    Servelet2

--- Project/OqFazer/src/br/com/fatec/oqfazer/web/servelet/Servelet1.java
+++ Project/OqFazer/src/br/com/fatec/oqfazer/web/servelet/Servelet1.java
@@ -1,5 +1,35 @@
 package br.com.fatec.oqfazer.web.servelet;
 
-public class Servelet1 {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servelet1 extends HttpServlet {
+
+	/** */
+	private static final long serialVersionUID = 8193598628287813449L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println(req.getParameter("nome"));
+		resp.getWriter().println("Servlet GET 1");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("Servlet POST 1");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet 1 - init");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Servlet 1 - destroy");
+	}
 }
