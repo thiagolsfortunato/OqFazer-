b2bca8ac54f5dfea7c0da3688edd4ac16a0466e4
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2016-04-23 22:11:01 -0300

    Update event.html and event.js

--- Project/OqFazer/WebContent/web_app/js/event/event.js
+++ Project/OqFazer/WebContent/web_app/js/event/event.js
@@ -1,50 +1,50 @@
 var app = angular.module('fatec');
 
-app.controller('GrupoController', function($scope, $http, $timeout) {
+app.controller('EventController', function($scope, $http, $timeout) {
 
-	var urlPath = "http://localhost:8585/projeto_exemplo/Grupo!";
-	TelaHelper.tela = 'grupo';
+	var urlPath = "http://localhost:8585/oqfazer/Event!";
+	TelaHelper.tela = 'event';
 	$scope.grupos = [];
 	$scope.currentPage = 1;
 	$scope.itemsPerPage = 5
 	$scope.grupo = {};
 
-	$scope.loadGrupos = function() {
-		$http.get(urlPath + 'listar.action', {
+	$scope.loadEvent = function() {
+		$http.get(urlPath + 'searchAll.action', {
 			cache : false
 		}).success(function(response) {
-	    	carregarLista(response);
+	    	buildList(response);
 		});
 	};
 
-	$scope.salvar = function() {
+	$scope.insert = function() {
 		var data = {contexto : {
-			grupo : $scope.grupo
+			event : $scope.event
 		}};
 		
 		var data1 = JSON.stringify(data);
 		jQuery.ajax({
-		    url: urlPath + 'salvar.action',
+		    url: urlPath + 'insert.action',
 		    data: data1,
 		    dataType: 'json',
 		    contentType: 'application/json',
 		    type: 'POST',
 		    async: true,
 		    success: function (response) {
-		        $scope.cancelarModal();
-		    	carregarLista(response);
+		        $scope.cancelModal();
+		    	buildList(response);
 		    }
 		});
 	};
 	
-	$scope.deletar = function(id) {
+	$scope.deleta = function(id) {
 		var data = {contexto : {
-			grupo : {id : id}
+			event : {id : id}
 		}};
 		
 		var data1 = JSON.stringify(data);
 		jQuery.ajax({
-		    url: urlPath + 'deletar.action',
+		    url: urlPath + 'delete.action',
 		    data: data1,
 		    dataType: 'json',
 		    contentType: 'application/json',
@@ -52,50 +52,50 @@ app.controller('GrupoController', function($scope, $http, $timeout) {
 		    async: false,
 		    success: function (response) {
 		    	$scope.id = null;
-		    	carregarLista(response);
+		    	buildLista(response);
 		    }
 		});
 	}
 	
-	$scope.abrirModal = function(id) {
+	$scope.openModal = function(id) {
 		if (id) {
 			var data = {contexto : {
-				grupo : {id : id}
+				event : {id : id}
 			}};
 
 			var data1 = JSON.stringify(data);
 			jQuery.ajax({
-			    url: urlPath + 'editar.action',
+			    url: urlPath + 'update.action',
 			    data: data1,
 			    dataType: 'json',
 			    contentType: 'application/json',
 			    type: 'POST',
 			    async: false,
 			    success: function (response) {
-			        $scope.grupo = response.contexto.grupo;
+			        $scope.event = response.contexto.event;
 			    }
 			});
 		}
 		jQuery('#modalForm').modal('show');
 	};
 
-	$scope.cancelarModal = function() {
-		$scope.grupo = {};
-		fecharModal();
+	$scope.cancelModal = function() {
+		$scope.event = {};
+		closeModal();
 	};
 
-	function carregarLista(response) {
-		$scope.grupos = response.contexto.grupos;
+	function buildLista(response) {
+		$scope.event = response.contexto.event;
 		$scope.currentPage = 1;
 		$scope.$applyAsync();
 	}
 	
-	function fecharModal() {
+	function closeModal() {
 		jQuery('#modalForm').modal('hide');
 	};
 	
 	setTimeout(function() {
-		$scope.loadGrupos();
+		$scope.loadEvents();
 	}, 0);
 	
 });
